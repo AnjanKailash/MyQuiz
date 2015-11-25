8a325adc358c65673710786dcbbe431a93fa6710
AnjanKailash <anjan.kailash@gmail.com>
2015-11-26 01:44:54 +0530

    upto categories, questions and a sample results page

--- quizes/urls.py
+++ quizes/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     # ex: /quizes/
     url(r'^$', views.index, name='index'),
     # ex: /quizes/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<category_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /quizes/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /quizes/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.submit, name='submit'),
+    url(r'^(?P<quiz_id>[0-9]+)/results/$', views.results, name='results'),
+    # # ex: /quizes/5/vote/
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.submit, name='submit'),
 ]
   
